2fe32d14577cd79292ea65c767553c9d21cb3c93
Esmail <113830751+Esmail-ibraheem@users.noreply.github.com>
2024-05-05 22:47:14 +0300

    Update model.py

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ from time import perf_counter
 import warnings
 from typing import List, Optional, Tuple, Union 
    
-import torch  
+import torch   
 import torch.nn.functional as F
 import torch.utils.checkpoint  
 from torch import nn
