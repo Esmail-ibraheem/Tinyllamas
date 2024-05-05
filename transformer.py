a55bae6776d2d83523f68ed7eacfe0899352a68a
Esmail <113830751+Esmail-ibraheem@users.noreply.github.com>
2024-05-05 07:55:51 +0300

    Update transformer.py

--- transformer.py
+++ transformer.py
@@ -3,7 +3,7 @@ import math
 import torch                   
 import torch.nn as nn                         
                          
-@dataclass                
+@dataclass                 
 class TransformerConfig:          
     source_vocab_size: int       
     target_vocab_size: int     
