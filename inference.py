4c83c2dbd982c48fd5a7f81d5d456f69b0bc6ab6
Esmail <113830751+Esmail-ibraheem@users.noreply.github.com>
2024-05-16 14:56:51 +0300

    Update inference.py

--- inference.py
+++ inference.py
@@ -6,7 +6,7 @@ from config import LlamaConfig
 
 from sentencepiece import SentencePieceProcessor
 import time
-import json  
+import json   
 from tqdm import tqdm
 from pathlib import Path 
 from typing import Optional
