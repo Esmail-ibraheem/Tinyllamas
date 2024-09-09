e43adbc029f0b17098368b00eb0e43857bc8b395
Esmail <113830751+Esmail-ibraheem@users.noreply.github.com>
2024-09-09 16:21:34 +0300

    Karpathy's llama.c rebuild to python

--- Tinyllama/configurator.py
+++ Tinyllama/configurator.py
@@ -1,19 +1,3 @@
-"""
-Poor Man's Configurator. Probably a terrible idea. Example usage:
-$ python train.py config/override_file.py --batch_size=32
-this will first run config/override_file.py, then override batch_size to 32
-
-The code in this file will be run as follows from e.g. train.py:
->>> exec(open('configurator.py').read())
-
-So it's not a Python module, it's just shuttling this code away from train.py
-The code in this script then overrides the globals()
-
-I know people are not going to love this, I just really dislike configuration
-complexity and having to prepend config. to every single variable. If someone
-comes up with a better simple Python solution I am all ears.
-"""
-
 import sys
 from ast import literal_eval
 
